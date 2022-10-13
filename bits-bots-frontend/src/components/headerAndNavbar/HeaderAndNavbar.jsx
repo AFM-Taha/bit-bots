b6d8f0f3ca27f63eb167e7c17c1a75a4cbec8706
MD TAHA <taha.iu.bd@gmail.com>
2022-10-13 20:54:10 +0600

    sone err solved

--- bits-bots-frontend/src/components/headerAndNavbar/HeaderAndNavbar.jsx
+++ bits-bots-frontend/src/components/headerAndNavbar/HeaderAndNavbar.jsx
@@ -3,76 +3,76 @@ import {
   faShoppingCart,
   faSignOut,
   faUser,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 
-import { signOut } from "../../redux/features/user/userSlice";
+import { signOut } from '../../redux/features/user/userSlice'
 
-import logo from "../../assets/img/logo.png";
+import logo from '../../assets/img/logo.png'
 
 const HeaderAndNavbar = () => {
-  const { cart } = useSelector((state) => state.cart);
+  const { cart } = useSelector((state) => state.cart)
   const { isLoading, platforms, error } = useSelector(
     (state) => state.platforms
-  );
-  const navigate = useNavigate();
+  )
+  const navigate = useNavigate()
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const handleLogOut = () => {
-    dispatch(signOut());
-    navigate("/login");
-  };
+    dispatch(signOut())
+    navigate('/login')
+  }
 
   return (
     <div>
       {/* <!-- ============   1. Header area start   ============= --> */}
-      <header className="header_area ">
-        <div className="container">
-          <div className="header_text">
-            <div className="logo">
-              <Link to="/">
-                <img src={logo} alt="images" />
+      <header className='header_area '>
+        <div className='container'>
+          <div className='header_text'>
+            <div className='logo'>
+              <Link to='/'>
+                <img src={logo} alt='images' />
               </Link>
             </div>
-            <div className="search">
-              <div className="desktopVersion_search d-md-block d-none">
-                <form action="index.html" method="POST">
-                  <input type="text" name="search" placeholder="Search.." />
+            <div className='search'>
+              <div className='desktopVersion_search d-md-block d-none'>
+                <form action='index.html' method='POST'>
+                  <input type='text' name='search' placeholder='Search..' />
                   <button>
                     <FontAwesomeIcon icon={faSearch} />
                   </button>
                 </form>
               </div>
-              <div className="mobile_version_search  d-md-none d-block">
-                <div id="search_popup" className="content_pop">
-                  <div className="popup_w searchpopup">
-                    <div className="search_big icon-search_icon">
+              <div className='mobile_version_search  d-md-none d-block'>
+                <div id='search_popup' className='content_pop'>
+                  <div className='popup_w searchpopup'>
+                    <div className='search_big icon-search_icon'>
                       <form
-                        role="search"
-                        id="search-form"
-                        className="search-form"
-                        action=""
+                        role='search'
+                        id='search-form'
+                        className='search-form'
+                        action=''
                       >
                         <input
-                          type="text"
-                          name="s"
-                          id="keyword"
-                          onkeyup="fetch()"
-                          placeholder="Search Dawa Life Sciences"
+                          type='text'
+                          name='s'
+                          id='keyword'
+                          onkeyup='fetch()'
+                          placeholder='Search Dawa Life Sciences'
                         />
                         <button>
                           <FontAwesomeIcon icon={faSearch} />
                         </button>
                       </form>
                       <button
-                        id="closesearch"
-                        title="Close (Esc)"
-                        type="button"
-                        className="mfp-close"
+                        id='closesearch'
+                        title='Close (Esc)'
+                        type='button'
+                        className='mfp-close'
                       >
                         ×
                       </button>
@@ -82,46 +82,46 @@ const HeaderAndNavbar = () => {
                 </div>
               </div>
             </div>
-            <div className="cart">
+            <div className='cart'>
               <ul>
-                <li className="position-relative">
-                  <Link to="/shopping-cart">
+                <li className='position-relative'>
+                  <Link to='/shopping-cart'>
                     <FontAwesomeIcon icon={faShoppingCart} />
                   </Link>
-                  {cart && <p className="cart-count ">{cart?.length}</p>}
+                  {cart && <p className='cart-count '>{cart?.length}</p>}
                 </li>
                 <li>
-                  <Link to="/my-profile">
+                  <Link to='/my-profile'>
                     <FontAwesomeIcon icon={faUser} />
                   </Link>
                 </li>
                 <li>
                   <p
                     style={{
-                      color: "#fff",
-                      cursor: "pointer",
-                      fontSize: "30px",
-                      marginLeft: "10px",
+                      color: '#fff',
+                      cursor: 'pointer',
+                      fontSize: '30px',
+                      marginLeft: '10px',
                     }}
                     onClick={handleLogOut}
                   >
                     <FontAwesomeIcon icon={faSignOut} />
                   </p>
                 </li>
-                <li className="d-md-none d-block">
-                  <Link to="/" className="searchoption-none">
+                <li className='d-md-none d-block'>
+                  <Link to='/' className='searchoption-none'>
                     <FontAwesomeIcon icon={faSearch} />
                   </Link>
                 </li>
 
                 {/* <!-- mobile humber menu area start  --> */}
                 <li
-                  className="rakibHumbergarMenu d-md-none d-block"
-                  onClick="myFunction(this)"
+                  className='rakibHumbergarMenu d-md-none d-block'
+                  onClick='myFunction(this)'
                 >
-                  <div className="bar1"></div>
-                  <div className="bar2"></div>
-                  <div className="bar3"></div>
+                  <div className='bar1'></div>
+                  <div className='bar2'></div>
+                  <div className='bar3'></div>
                 </li>
                 {/* <!-- mobile humber menu area End  --> */}
               </ul>
@@ -132,17 +132,19 @@ const HeaderAndNavbar = () => {
       {/* <!-- ============   1. Header area end   ============= --> */}
 
       {/* <!-- ============   2. Menu area start   ============= --> */}
-      <menu className="menu">
-        <div className="container">
-          <div className="menuText">
+      <menu className='menu'>
+        <div className='container'>
+          <div className='menuText'>
             <ul>
               {platforms?.map((platform) => {
-                const { id, name } = platform;
+                const { id, name } = platform
                 return (
-                  <li key={id}>
-                    <Link to={`/platforms/${id}`}>{name}</Link>
+                  <li className='nav-li' key={id}>
+                    <NavLink activeClassName='active' to={`/platforms/${id}`}>
+                      <li> {name} </li>
+                    </NavLink>
                   </li>
-                );
+                )
               })}
             </ul>
           </div>
@@ -150,7 +152,7 @@ const HeaderAndNavbar = () => {
       </menu>
       {/* <!-- ============   2. Menu area end   ============= --> */}
     </div>
-  );
-};
+  )
+}
 
-export default HeaderAndNavbar;
+export default HeaderAndNavbar
--- bits-bots-frontend/src/pages/register/Register.jsx
+++ bits-bots-frontend/src/pages/register/Register.jsx
@@ -17,12 +17,14 @@ const Register = () => {
   const handleRegister = (e) => {
     setErrorText('')
     e.preventDefault()
-    const userName = e.target.name.value
+    const firstName = e.target.firstname.value
+    const lastName = e.target.lastname.value
+    const userName = firstName + ' ' + lastName
     const email = e.target.email.value
     const password = e.target.password.value
-    if (!userName || !email || !password)
-      return setErrorText('Please input all information')
-
+    const confirmPassword = e.target.password.value
+    if (confirmPassword !== password)
+      return setErrorText('New password and Confirm Password Does Not match')
     const userData = { userName, email, password }
 
     dispatch(registerNewUser(userData))
@@ -41,62 +43,216 @@ const Register = () => {
   return (
     <section class='landingPage'>
       <div class='container'>
-        <div class='landing_page_text'>
-          <form onSubmit={handleRegister}>
+        <form onSubmit={handleRegister}>
+          <div class='landing_page_text'>
             <div class='row'>
-              <div class='col-md-6 offset-md-3 col-lg-4 offset-lg-4'>
+              <div class='col-md-10 offset-md-1 col-lg-8 offset-lg-2'>
                 {/* <!-- landing page start  --> */}
                 <div class='landingFormArea'>
                   <Link to='/'>
                     <img src={logo} alt='images' />
                   </Link>
-                  <div class='landingInputArea'>
-                    {/* <!-- single item area start --> */}
-                    <div class='landingInputArea_single'>
-                      <input type='text' name='name' placeholder='NAME' />
-                    </div>
+                  <div class='inputCart_text'>
+                    {/* <!-- single main cart start  --> */}
+                    <div class='input_single_cart_main'>
+                      <h2> My Profile</h2>
+                      <form action='' method='POST'>
+                        <div class='row g-4'>
+                          {/* <!-- single input area  --> */}
+                          <div class='col-md-6'>
+                            <div class='input_single_cart'>
+                              <input
+                                class='form-control'
+                                type='text'
+                                name='firstname'
+                                placeholder='First Name'
+                                required
+                              />
+                            </div>
+                          </div>
+                          {/* <!-- single input area  --> */}
+                          <div class='col-md-6'>
+                            <div class='input_single_cart'>
+                              <input
+                                class='form-control'
+                                type='text'
+                                name='lastname'
+                                placeholder='Last Name'
+                                required
+                              />
+                            </div>
+                          </div>
+                          {/* <!-- single input area  --> */}
+                          <div class='col-md-12'>
+                            <div class='input_single_cart'>
+                              <input
+                                class='form-control'
+                                type='text'
+                                name='StreetAdress'
+                                placeholder='Street Adress'
+                                required
+                              />
+                            </div>
+                          </div>
+                          {/* <!-- single input area  --> */}
+                          <div class='col-md-6'>
+                            <div class='input_single_cart'>
+                              <input
+                                class='form-control'
+                                type='text'
+                                name='zipcode'
+                                placeholder='Zip code'
+                                required
+                              />
+                            </div>
+                          </div>
 
-                    <div class='landingInputArea_single'>
-                      <input type='email' name='email' placeholder='EMAIL' />
-                    </div>
-                    {/* <!-- single item area End --> */}
-                    {/* <!-- single item area start --> */}
-                    <div class='landingInputArea_single'>
-                      <input
-                        type='password'
-                        name='password'
-                        placeholder='PASSWORD'
-                      />
-                    </div>
-                    {/* <!-- single item area End --> */}
-                    {/* <!-- single item area start --> */}
+                          {/* <!-- single input area  --> */}
+                          <div class='col-md-6'>
+                            <div class='input_single_cart'>
+                              <input
+                                class='form-control'
+                                type='text'
+                                name='city'
+                                placeholder='City'
+                                required
+                              />
+                            </div>
+                          </div>
 
-                    {errorText && <p style={{ color: 'red' }}>{errorText}</p>}
+                          {/* <!-- single input area  --> */}
+                          <div class='col-md-6'>
+                            <div class='input_single_cart'>
+                              <input
+                                class='form-control'
+                                type='email'
+                                name='email'
+                                placeholder='Email (user id)'
+                                required
+                              />
+                            </div>
+                          </div>
 
-                    <div class='landingInputArea_singleBtn'>
-                      <input class='inputBtn' type='submit' value='REGISTER' />
+                          {/* <!-- single input area  --> */}
+                          <div class='col-md-6'>
+                            <div class='input_single_cart'>
+                              <input
+                                class='form-control'
+                                type='text'
+                                name='phone'
+                                placeholder='Phone'
+                                required
+                              />
+                            </div>
+                          </div>
+                          {/* <!-- single input area  --> */}
+                          <div class='col-md-6'>
+                            <div class='input_single_cart'>
+                              <input
+                                class='form-control'
+                                type='password'
+                                name='phone'
+                                placeholder='New your password'
+                                required
+                              />
+                            </div>
+                          </div>
+                          {/* <!-- single input area  --> */}
+                          <div class='col-md-6'>
+                            <div class='input_single_cart'>
+                              <input
+                                class='form-control'
+                                type='confirmPassword'
+                                placeholder='Confirm your password'
+                                name='password'
+                                required
+                              />
+                            </div>
+                          </div>
+                          {/* <!-- btn area  input area  --> */}
+                          <div class='col-md-12'>
+                            <div class='input_single_cartBtn'>
+                              {errorText && (
+                                <p style={{ color: 'red' }}>{errorText}</p>
+                              )}
+                              <input
+                                class='btn-style'
+                                type='submit'
+                                value='REGISTER'
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </form>
                     </div>
-                    {/* <!-- single item area End --> */}
-                    {/* <!-- single item area start --> */}
-                    <div class='landingInputArea_singleBtn'>
-                      <button
-                        class='inputBtn'
-                        onClick={() => navigate('/login')}
-                      >
-                        ALREADY USER? <b>LOGIN</b>
-                      </button>
-                    </div>
-                    {/* <!-- single item area End --> */}
+                    {/* <!-- single main cart End  --> */}
                   </div>
-                  <Link to='/forgot-password'>Forgot password?</Link>
                 </div>
                 {/* <!-- landing page End  --> */}
               </div>
             </div>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
     </section>
+    // <section class='landingPage'>
+    //   <div class='container'>
+    //     <div class='landing_page_text'>
+    //       <form onSubmit={handleRegister}>
+    //         <div class='row'>
+    //           <div class='col-md-6 offset-md-3 col-lg-4 offset-lg-4'>
+    //             {/* <!-- landing page start  --> */}
+    //             <div class='landingFormArea'>
+    //               <Link to='/'>
+    //                 <img src={logo} alt='images' />
+    //               </Link>
+    //               <div class='landingInputArea'>
+    //                 {/* <!-- single item area start --> */}
+    //                 <div class='landingInputArea_single'>
+    //                   <input type='text' name='name' placeholder='NAME' />
+    //                 </div>
+
+    //                 <div class='landingInputArea_single'>
+    //                   <input type='email' name='email' placeholder='EMAIL' />
+    //                 </div>
+    //                 {/* <!-- single item area End --> */}
+    //                 {/* <!-- single item area start --> */}
+    //                 <div class='landingInputArea_single'>
+    //                   <input
+    //                     type='password'
+    //                     name='password'
+    //                     placeholder='PASSWORD'
+    //                   />
+    //                 </div>
+    //                 {/* <!-- single item area End --> */}
+    //                 {/* <!-- single item area start --> */}
+
+    //                 {errorText && <p style={{ color: 'red' }}>{errorText}</p>}
+
+    //                 <div class='landingInputArea_singleBtn'>
+    //                   <input class='inputBtn' type='submit' value='REGISTER' />
+    //                 </div>
+    //                 {/* <!-- single item area End --> */}
+    //                 {/* <!-- single item area start --> */}
+    //                 <div class='landingInputArea_singleBtn'>
+    //                   <button
+    //                     class='inputBtn'
+    //                     onClick={() => navigate('/login')}
+    //                   >
+    //                     ALREADY USER? <b>LOGIN</b>
+    //                   </button>
+    //                 </div>
+    //                 {/* <!-- single item area End --> */}
+    //               </div>
+    //               <Link to='/forgot-password'>Forgot password?</Link>
+    //             </div>
+    //             {/* <!-- landing page End  --> */}
+    //           </div>
+    //         </div>
+    //       </form>
+    //     </div>
+    //   </div>
+    // </section>
   )
 }
 
